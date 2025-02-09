a817f86619b1d8803d1a7ea62a517e5f8236bff2
Raktim Biswas <raktimb181@gmail.com>
2025-02-09 18:08:54 +0530

    Type Hinting Update

--- php/TypeHinting.php
+++ php/TypeHinting.php
@@ -1,7 +1,45 @@
 <!-- Type-Hinting and Type Declaration are the same thing -->
 <?php
-function sum(int $a, int $b) { // Type-Hinting
-    echo $a + $b + 1;
+// function sum(int $a, int $b) { // Type-Hinting
+//     echo $a + $b + 1;
+// }
+// sum(55, 45);
+
+// class hello{
+//     public function sayhello(){
+//         echo "Hello";
+//     }
+// }
+// class bye{
+//     public function saybye(){
+//         echo "Bye";
+//     }
+// }
+// function wow(hello $c){ //Type Hinting
+//     $c->sayhello();
+// }
+// $test = new hello();
+// wow($test);
+
+class school{
+    public function getNames(student $names){ //Type Hinting
+        echo "<ul>";
+        foreach($names->Names() as $name){
+            echo "<li>". $name . "</li>";
+        }
+        echo "</ul>";
+    }
 }
-sum(55, 45);
+class student{
+    public function Names(){
+        return ['John', 'Doe', 'Smith'];
+    }
+}
+class library{
+
+}
+$lib = new library();
+$stu = new student();
+$sch = new school();
+$sch->getNames($stu);
 ?>
